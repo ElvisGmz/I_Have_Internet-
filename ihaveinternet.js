173e4b960ac6a8d4768ccc0e8b2d2d5fc789ce72
Elvis Gomez <elvis.gomez@elaniin.com>
2021-07-05 20:06:31 -0600

    ✨ Update Service Workers

--- ihaveinternet.js
+++ ihaveinternet.js
@@ -1,29 +1,44 @@
-var cacheName = 'ihaveinternet';
+var cacheName = ["ihaveinternet_1.0.1"];
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/index.js',
-  '/assets/undraw_broadcast.svg',
-  '/assets/undraw_signal_searching.svg',
-  '/assets/undraw_Loading.svg',
-  '/assets/dude.jpg',
-  '/assets/icon.svg'
+  "/",
+  "/index.html",
+  "/style.css",
+  "/index.js",
+  "/assets/undraw_broadcast.svg",
+  "/assets/undraw_signal_searching.svg",
+  "/assets/undraw_Loading.svg",
+  "/assets/dude.jpg",
+  "/assets/icon.svg",
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+// On version update, remove old cached files
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+      caches.keys().then(cacheNames => {
+          return Promise.all(
+              cacheNames.map(thisCacheName => {
+                  if (!cacheName.includes(thisCacheName)) {
+                      return caches.delete(thisCacheName);
+                  }
+              })
+          );
+      })
+  );
+});
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
